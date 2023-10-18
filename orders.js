44edbed398479fbcbe5693a20fc78e7afa902c95
Gimnastiar18 <ariegimnastiar18@gmail.com>
2023-10-18 21:55:26 +0700

    Done!
    Update web kosan!

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Rizal',
+        productNumber: '085743456667',
+        paymentStatus: 'Tranfer',
+        status: 'Pending'
+    },
+    {
+        productName: 'Rival',
+        productNumber: '0812345678910',
+        paymentStatus: 'M Banking',
+        status: 'Declined'
+    },
+    {
+        productName: 'Ando',
+        productNumber: '0850123456789',
+        paymentStatus: 'Qris',
+        status: 'Succeed'
+    },
+]
